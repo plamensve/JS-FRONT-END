0d0c402dda83c6f768b01796fb55dd6823ec80c1
Plamen Svetoslavov <svetoslavov.plamen@gmail.com>
2024-12-09 22:43:16 +0200

    ADD: Some solutions

---
+++ JS Syntax Fundamentals – Lab/02. Student Information.js
@@ -0,0 +1,5 @@
+function printName(name, age, grade){
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`)
+}
+
+printName('John', 15, 5.54678)
---
+++ JS Syntax Fundamentals – Lab/03. Excellent Grade.js
@@ -0,0 +1,10 @@
+function grade(number){
+    let excellent = 5.49
+    if (number > excellent) {
+        console.log('Excellent')
+    } else {
+        console.log('Not excellent')
+    }
+}
+
+grade(4.35)
